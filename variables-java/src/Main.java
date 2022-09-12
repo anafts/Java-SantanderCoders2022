d0cd8fc78d6cb0128a2e0365119325d6c2d77f5b
Ana Freitas <ana.freitaas16@gmail.com>
2022-09-12 11:35:44 -0300

    exercicio de variaveis em java

---
+++ variables-java/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String nome = "Lucas";
+        int idade;
+        idade = 23;
+        String profissao = "Programador";
+        String mascote = "Porquinha Luna";
+        System.out.println(nome);
+        System.out.println(idade);
+        System.out.println(profissao);
+        System.out.println(mascote);
+
+    }
+}
